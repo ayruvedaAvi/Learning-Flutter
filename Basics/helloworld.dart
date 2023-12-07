07ccb6e607433e921f9d2bc8cb349c1f1321af92
hitmanOg <neupaneabhishek20@gmail.com>
2023-12-07 08:56:39 +0545

    Organized a bit

--- Basics/helloworld.dart
+++ Basics/helloworld.dart
@@ -1,12 +1,9 @@
-
-
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(
     const MaterialApp(
       home: Scaffold(
-        backgroundColor: Color.fromARGB(255, 113, 9, 148),
         body: Center(
           child: Text('Hello world!'),
         ),
