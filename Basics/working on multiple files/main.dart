119735fb488f993ddf6938496a6437871dd03743
hitmanOg <neupaneabhishek20@gmail.com>
2023-12-26 23:11:02 +0545

    learnt working on different files

--- Basics/working on multiple files/main.dart
+++ Basics/working on multiple files/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:first_app/gradient_container.dart';    //connecting the file
+import 'package:first_app/gradient_container.dart';    //connecting the other file containing gradient_container.dart
+                                                      //First app is the name of the projectt. that is how you give a path
 
 void main() {
   runApp(
