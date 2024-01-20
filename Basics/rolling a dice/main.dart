20e92dde9bcb125c735a8b9fb575660386b9e432
hitmanOg <neupaneabhishek20@gmail.com>
2024-01-20 11:24:52 +0545

    added the dice roller codes

---
+++ Basics/rolling a dice/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:first_app/gradient_container.dart';    //connecting the file
+
+void main() {
+  const color = [
+    Color.fromARGB(255, 255, 191, 64),
+    Color.fromARGB(255, 196, 15, 190)
+  ];
+  runApp(
+    const MaterialApp(
+      home: Scaffold(
+        body: GradientContainer(color), 
+      ),
+    ),
+  );
+}
+
