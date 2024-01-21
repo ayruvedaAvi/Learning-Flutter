426ccd899fea04f3b9cdd3ae0a5ce3e8a43e8087
hitmanOg <neupaneabhishek20@gmail.com>
2024-01-21 18:57:12 +0545

    fixed the in between text

--- basic_quiz/lib/image_container.dart
+++ basic_quiz/lib/image_container.dart
@@ -4,23 +4,33 @@ class ImageContainer extends StatelessWidget {
   const ImageContainer({super.key});
   @override
   Widget build(context) {
-    return Column(
-      mainAxisSize: MainAxisSize.min,
-      children: [
-        Image.asset(
-          'assets/images/quiz-logo.png',
-          width: 300,
-          height: 300,
+    return Column(mainAxisSize: MainAxisSize.min, children: [
+      Image.asset(
+        'assets/images/quiz-logo.png',
+        width: 300,
+        height: 300,
+      ),
+      const SizedBox(
+        height: 50,
+      ),
+      const Text(
+        "Learn Flutter the fun way!",
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 20,
         ),
-        const SizedBox(height: 20,),
-        OutlinedButton(
-          onPressed: ()=>{},
-          style: OutlinedButton.styleFrom(
-            foregroundColor: const Color.fromARGB(255, 255, 255, 255),
-            textStyle: const TextStyle(fontSize: 15),
-          ),
-          child: const Text("Start Quiz"),
-        )
+      ),
+      const SizedBox(
+        height: 20,
+      ),
+      OutlinedButton(
+        onPressed: () => {},
+        style: OutlinedButton.styleFrom(
+          foregroundColor: const Color.fromARGB(255, 255, 255, 255),
+          textStyle: const TextStyle(fontSize: 15),
+        ),
+        child: const Text("Start Quiz"),
+      )
     ]);
   }
 }
