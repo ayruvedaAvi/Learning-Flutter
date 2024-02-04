1c9934154f6139c8b33d3b4601fb77c6099d12cb
hitmanOg <neupaneabhishek20@gmail.com>
2024-02-04 21:30:14 +0545

    added other screens in the quiz

--- basic_quiz/lib/main.dart
+++ basic_quiz/lib/main.dart
@@ -1,27 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:basic_quiz/start_screen.dart';
-
-const start = Alignment.topLeft;
-const end = Alignment.bottomRight;
-
+import 'package:basic_quiz/quiz.dart';
 void main() {
   runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(248, 255, 234, 0),
-                Color.fromARGB(233, 170, 0, 142)
-              ],
-              begin: start,
-              end: end,
-            ),
-          ),
-          child: StartScreen(),
-        ),
-      ),
-    ),
+    const Quiz()
   );
 }
--- basic_quiz/lib/start_screen.dart
+++ basic_quiz/lib/start_screen.dart
@@ -3,7 +3,9 @@ import 'package:flutter/material.dart';
 const textColor = Color.fromARGB(255, 255, 198, 198);
 
 class StartScreen extends StatelessWidget {
-  const StartScreen({super.key});
+  const StartScreen(this.startQuiz, {super.key});
+
+  final void Function() startQuiz;
 
   @override
   Widget build(context) {
@@ -32,7 +34,7 @@ class StartScreen extends StatelessWidget {
         ),
         const SizedBox(height: 30),
         OutlinedButton.icon(
-          onPressed: () {},
+          onPressed: startQuiz,
           style: OutlinedButton.styleFrom(
             foregroundColor: textColor,
           ),
