562b53c560868ccd50e9b969c632ef99e88b18d7
hitmanOg <neupaneabhishek20@gmail.com>
2024-02-05 12:23:33 +0545

    Added questions data and model

---
+++ basic_quiz/lib/data/questions.dart
@@ -0,0 +1,49 @@
+import 'package:basic_quiz/models/quiz_questions.dart';
+
+const questions = [
+  QuizQuestion(
+    'Who is the richest woman in Nepal?', 
+    [
+      'Kp ko budi',
+      'Arju Rana', 
+      'Binod Chaudhary ko budi', 
+      'Jamuna Gurung',
+    ],
+  ),
+  QuizQuestion(
+    'What is the average IQ of Nepali people?', 
+    [
+      'Room temp in F = 72',
+      'Room temp in ^C = 20', 
+      'Albert Einstein ko bhanda ali kam = dherai', 
+      'Total no of districts in Nepal = 77',
+    ],
+  ),
+  QuizQuestion(
+    'Why is Nepal backwards in Development and stuff?', 
+    [
+      'Politicians',
+      'China le garda ho', 
+      'India le garda ho', 
+      'Masu Bhat',
+    ],
+  ),
+  QuizQuestion(
+    'Balen dai ko shrimati ko name k ho?', 
+    [
+      'Nephop ko Rani',
+      'Mayorni saheb', 
+      'Sadikshya Kafle Shah', 
+      'Sabeena Kafle Shah',
+    ],
+  ),
+  QuizQuestion(
+    'What is the Water % of Nepal?', 
+    [
+      'World ko 2nd richest. WTF IS SALT WATER????',
+      'Hydro Elrctricity goes brrrr. 50.35%', 
+      'Melamchi aayo ktm ma. 5.78%', 
+      'Saptakoshi dekhya chha katro chha? 2.8%',
+    ],
+  )
+];
