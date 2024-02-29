2e60ffc0aee8dd31722c17eb92fe98f2f72d012c
hitmanOg <neupaneabhishek20@gmail.com>
2024-02-29 22:54:29 +0545

    yolo

--- expense_tracker/lib/widgets/expenses_list/expenses_list.dart
+++ expense_tracker/lib/widgets/expenses_list/expenses_list.dart
@@ -3,7 +3,7 @@ import 'package:expense_tracker/widgets/expenses_list/expense_item.dart';
 import 'package:flutter/material.dart';
 
 class ExpensesList extends StatelessWidget {
-  const ExpensesList({super.key, required this.expenses});
+  const ExpensesList({super.key, required this.expenses});  //yolo
   final List<Expense> expenses;
 
   @override
