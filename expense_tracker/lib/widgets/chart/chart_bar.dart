d7dd152b8e96427587152cd8fba17a2644bf3baf
hitmanOg <neupaneabhishek20@gmail.com>
2024-03-02 13:52:50 +0545

    added chart-bar

---
+++ expense_tracker/lib/widgets/chart/chart_bar.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class ChartBar extends StatelessWidget {
+  const ChartBar({
+    super.key,
+    required this.fill,
+  });
+
+  final double fill;
+
+  @override
+  Widget build(BuildContext context) {
+    final isDarkMode =
+        MediaQuery.of(context).platformBrightness == Brightness.dark;
+    return Expanded(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 4),
+        child: FractionallySizedBox(
+          heightFactor: fill,
+          child: DecoratedBox(
+            decoration: BoxDecoration(
+              shape: BoxShape.rectangle,
+              
