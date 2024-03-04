d03a9d16b33da2420305acb18ddfc9fbb6f44af7
hitmanOg <neupaneabhishek20@gmail.com>
2024-03-04 21:32:06 +0545

    Responsiveness: Fixing the camera issue

--- expense_tracker/lib/widgets/expenses.dart
+++ expense_tracker/lib/widgets/expenses.dart
@@ -25,6 +25,7 @@ class _ExpensesState extends State<Expenses> {
 
   void _addExpenseOverlay() {
     showModalBottomSheet(
+      useSafeArea: true,
       isScrollControlled: true,
       context: context,
       builder: (ctx) => NewExpense(onAddExpense: _addExpense),
