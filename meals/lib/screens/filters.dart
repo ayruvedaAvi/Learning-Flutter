3f1fa1c90fcbdb9c1e348d71ffb2938dbf3a47bb
hitmanOg <neupaneabhishek20@gmail.com>
2024-04-05 18:00:26 +0545

    added the init method on filter screen

--- meals/lib/screens/filters.dart
+++ meals/lib/screens/filters.dart
@@ -10,6 +10,10 @@ enum Filter {
 }
 
 class FiltersScreen extends StatefulWidget {
+  const FiltersScreen({super.key, required this.currentFilters});
+
+  final Map<Filter, bool> currentFilters;
+
   @override
   State<FiltersScreen> createState() {
     return _FiltersScreenState();
@@ -21,6 +25,16 @@ class _FiltersScreenState extends State<FiltersScreen> {
   var _lactoseFreeFilterSet = false;
   var _vegeterianFilterSet = false;
   var _veganFilterSet = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _glutenFreeFilterSet = widget.currentFilters[Filter.glutenFree]!;
+    _lactoseFreeFilterSet = widget.currentFilters[Filter.lactoseFree]!;
+    _vegeterianFilterSet = widget.currentFilters[Filter.vegeterian]!;
+    _veganFilterSet = widget.currentFilters[Filter.vegan]!;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
