0ef365a06626fcd764e3c685ef630092a66c40a3
hitmanOg <neupaneabhishek20@gmail.com>
2024-04-16 13:46:53 +0545

    added form to add new item

--- shopping_list/lib/widgets/grocery_list.dart
+++ shopping_list/lib/widgets/grocery_list.dart
@@ -1,14 +1,39 @@
 import 'package:flutter/material.dart';
 import 'package:shopping_list/data/dummy_items.dart';
+import 'package:shopping_list/widgets/new_item.dart';
 
-class GroceryList extends StatelessWidget {
+class GroceryList extends StatefulWidget {
   const GroceryList({super.key});
+
+  @override
+  State<GroceryList> createState() => _GroceryListState();
+}
+
+class _GroceryListState extends State<GroceryList> {
+  void _addItem() {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (ctx) => const NewItem(),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your groceries'),
+        actions: [
+          FilledButton(
+              onPressed: _addItem,
+              child: const Row(
+                children: [
+                  Text('Add more'),
+                  SizedBox(width: 10,),
+                  Icon(Icons.add),
+                ],
+              )),
+        ],
       ),
       body: ListView.builder(
         itemCount: groceryItems.length,
