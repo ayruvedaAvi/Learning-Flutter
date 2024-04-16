5a0437f5a817746c1c3d3192330b188079e50a86
hitmanOg <neupaneabhishek20@gmail.com>
2024-04-16 14:09:02 +0545

    form is ready

--- shopping_list/lib/widgets/new_item.dart
+++ shopping_list/lib/widgets/new_item.dart
@@ -1,4 +1,7 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:shopping_list/data/categories.dart';
 
 class NewItem extends StatefulWidget {
   const NewItem({super.key});
@@ -29,6 +32,42 @@ class _NewItemState extends State<NewItem> {
                 validator: (value) {
                   return 'Demo..';
                 },
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    child: TextFormField(
+                      decoration: const InputDecoration(
+                        label: Text('Quantity'),
+                      ),
+                      initialValue: '1',
+                    ),
+                  ),
+                  const SizedBox(
+                    width: 8,
+                  ),
+                  Expanded(
+                    child: DropdownButtonFormField(items: [
+                      for (final category in categories.entries)
+                        DropdownMenuItem(
+                          value: category.value,
+                          child: Row(
+                            children: [
+                              Container(
+                                width: 16,
+                                height: 16,
+                                color: category.value.color,
+                              ),
+                              const SizedBox(
+                                width: 6,
+                              ),
+                              Text(category.value.title)
+                            ],
+                          ),
+                        )
+                    ], onChanged: (value) {}),
+                  ),
+                ],
               ),
             ],
           ),
