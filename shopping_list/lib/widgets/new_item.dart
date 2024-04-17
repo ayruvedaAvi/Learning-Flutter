656b04c1fe5b16563946368b170f30f00ed1efcb
hitmanOg <neupaneabhishek20@gmail.com>
2024-04-17 23:26:47 +0545

    adding an item finished

--- shopping_list/lib/widgets/new_item.dart
+++ shopping_list/lib/widgets/new_item.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:shopping_list/data/categories.dart';
+import 'package:shopping_list/models/category.dart';
+import 'package:shopping_list/models/grocery_item.dart';
 
 class NewItem extends StatefulWidget {
   const NewItem({super.key});
@@ -13,10 +15,21 @@ class NewItem extends StatefulWidget {
 class _NewItemState extends State<NewItem> {
   final _formKey = GlobalKey<FormState>();
   var _enteredName = '';
+  var _enteredQuantity = 0.0;
+  var _selectedCategory = categories[Categories.carbs]!;
 
-  void _saveItem(){
-    _formKey.currentState!.validate();
-    _formKey.currentState!.save();
+  void _saveItem() {
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState!.save();
+      Navigator.of(context).pop(
+        GroceryItem(
+          id: DateTime.now().toString(),
+          name: _enteredName,
+          quantity: _enteredQuantity,
+          category: _selectedCategory,
+        ),
+      );
+    }
   }
 
   @override
@@ -32,23 +45,22 @@ class _NewItemState extends State<NewItem> {
           child: Column(
             children: [
               TextFormField(
-                maxLength: 50,
-                decoration: const InputDecoration(
-                  label: Text('Name'),
-                ),
-                validator: (value) {
-                  if (value == null ||
-                      value.isEmpty ||
-                      value.trim().length <= 1 ||
-                      value.trim().length > 50) {
-                    return "Must be between 1 and 50 chars";
-                  }
-                  return null;
-                },
-                onSaved: (value){
-                  _enteredName = value!;
-                }
-              ),
+                  maxLength: 50,
+                  decoration: const InputDecoration(
+                    label: Text('Name'),
+                  ),
+                  validator: (value) {
+                    if (value == null ||
+                        value.isEmpty ||
+                        value.trim().length <= 1 ||
+                        value.trim().length > 50) {
+                      return "Must be between 1 and 50 chars";
+                    }
+                    return null;
+                  },
+                  onSaved: (value) {
+                    _enteredName = value!;
+                  }),
               Row(
                 crossAxisAlignment: CrossAxisAlignment.end,
                 children: [
@@ -68,31 +80,42 @@ class _NewItemState extends State<NewItem> {
                         }
                         return null;
                       },
+                      onSaved: (value) {
+                        _enteredQuantity = double.parse(value!);
+                      },
                     ),
                   ),
                   const SizedBox(
                     width: 8,
                   ),
                   Expanded(
-                    child: DropdownButtonFormField(items: [
-                      for (final category in categories.entries)
-                        DropdownMenuItem(
-                          value: category.value,
-                          child: Row(
-                            children: [
-                              Container(
-                                width: 16,
-                                height: 16,
-                                color: category.value.color,
-                              ),
-                              const SizedBox(
-                                width: 6,
-                              ),
-                              Text(category.value.title)
-                            ],
-                          ),
-                        )
-                    ], onChanged: (value) {}),
+                    child: DropdownButtonFormField(
+                      value: _selectedCategory,
+                      items: [
+                        for (final category in categories.entries)
+                          DropdownMenuItem(
+                            value: category.value,
+                            child: Row(
+                              children: [
+                                Container(
+                                  width: 16,
+                                  height: 16,
+                                  color: category.value.color,
+                                ),
+                                const SizedBox(
+                                  width: 6,
+                                ),
+                                Text(category.value.title)
+                              ],
+                            ),
+                          )
+                      ],
+                      onChanged: (value) {
+                        setState(() {
+                          _selectedCategory = value!;
+                        });
+                      },
+                    ),
                   ),
                 ],
               ),
