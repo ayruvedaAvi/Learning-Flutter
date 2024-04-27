d57493bd33de6d04c7a117e3eff5284ff4412fe8
hitmanOg <neupaneabhishek20@gmail.com>
2024-04-27 13:41:05 +0545

    capture image widget added

--- favorite_places/lib/screens/add_place.dart
+++ favorite_places/lib/screens/add_place.dart
@@ -1,4 +1,5 @@
 import 'package:favorite_places/providers/user_places.dart';
+import 'package:favorite_places/widgets/image_input.dart';
 
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -50,6 +51,8 @@ class _AddPlaceScreenState extends ConsumerState<AddPlaceScreen> {
               style:
                   TextStyle(color: Theme.of(context).colorScheme.onBackground),
             ),
+            //Image input
+            const ImageInput(),
             const SizedBox(
               height: 20,
             ),
